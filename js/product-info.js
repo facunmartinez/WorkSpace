1f3c4fa1771bf3947f5bef7683fb40a3ea5bd55d
facunmartinez <famartinez023@gmail.com>
2020-09-10 18:20:15 -0300

    Agregue seccion de comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,6 @@
 var product = {};
 
+
 function showImagesGallery(array) {
 
     let htmlContentToAppend = "";
@@ -45,6 +46,83 @@ function showRelatedProducts(array) {
     }
 }
 
+function showComments(array) {
+    console.log("");
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let cmmts = array[i];
+        let score = "";
+        for (let i = 1; i <= cmmts.score; i++) {
+            score += `<span class="fa fa-star checked"></span>`
+        }
+        for (let i = cmmts.score + 1; i <= 5; i++) {
+            score += `<span class="fa fa-star"></span>`
+        }
+
+        htmlContentToAppend += `
+
+        <div class="comments-container">
+           <ul id="comments-list" class="comments-list">
+              <li>
+                 <div class="comment-main-level">
+                    <div class="comment-avatar"><img src="img/user.png" alt=""></div>
+                     <div class="comment-box">
+                         <div class="comment-head">
+                             <h6 class="comment-name">` + cmmts.user + `</a></h6>
+                             <span>` + cmmts.dateTime + `</span>
+                             <span class="score">` + score + `</span>
+                          </div>
+                          <div class="comment-content">` + cmmts.description + `</div>
+                     </div>
+                 </div>
+              </li>
+           </ul>
+        </div>
+
+
+      `
+
+        document.getElementById("comments").innerHTML = htmlContentToAppend;
+
+    }
+}
+
+function addCommment() {
+
+    let htmlContentToAppend = "";
+
+    htmlContentToAppend += `
+
+    <form>
+    <div class="comments-container">
+        <ul id="comments-list" class="comments-list">
+            <li>
+                <div class="comment-main-level">
+                    <!-- Avatar -->
+                    <div class="comment-avatar"><img src="img/user.png" alt=""></div>
+                    <!-- Contenedor del Comentario -->
+                    <div class="comment-box">
+                        <div class="comment-head">
+                            <h1 id="user-b" class="comment-name"></h1>
+                            <span id="HoraActual"></span>
+                        </div>
+                        <textarea type="text" class="md-textarea form-control" rows="3"></textarea>
+                        <button type="submit" class="btn btn-lg btn-primary btn-signin">Comentar</button>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</form>
+`
+    document.getElementById("addComm").innerHTML = htmlContentToAppend;
+    document.getElementById("user-b").innerHTML = localStorage.getItem("user");
+
+}
+
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -87,3 +165,34 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+
+            showComments(resultObj.data);
+            addCommment();
+
+
+        }
+    });
+});
+
+showTime();
+
+function showTime() {
+    myDate = new Date();
+    hours = myDate.getHours();
+    minutes = myDate.getMinutes();
+    seconds = myDate.getSeconds();
+
+    if (hours < 10) hours = 0 + hours;
+
+    if (minutes < 10) minutes = "0" + minutes;
+
+    if (seconds < 10) seconds = "0" + seconds;
+
+    $("#HoraActual").text(hours + ":" + minutes + ":" + seconds);
+    setTimeout("showTime()", 1000);
+}
